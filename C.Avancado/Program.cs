4fb85beeb3a0c65827853e8e586a613550d80b95
Leonardo Martins <leonardogm85@gmail.com>
2022-01-31 09:39:04 -0300

    Operadores e Casting

---
+++ C.Avancado/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace C.Avancado
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            // Modulo01.Exercicio1.Resposta.Executar();
+            // Modulo01.Exercicio2.Resposta.Executar();
+        }
+    }
+}
